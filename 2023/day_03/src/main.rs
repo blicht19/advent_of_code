a3b806ced02730413d5e6057f0e0a3ed54469c2e
lichtb <baileymlicht@gmail.com>
2024-02-10 14:20:04 -0600

    Day 3 part 1

--- 2023/day_03/src/main.rs
+++ 2023/day_03/src/main.rs
@@ -5,6 +5,10 @@ use regex::Regex;
 fn main() {
     let file_name = get_filename_arg();
     let input_vec = get_two_dimensional_vector(file_name.as_str());
+
+    let part_one_sum = part_one_sum(&input_vec);
+
+    println!("Part 1: {}", part_one_sum);
 }
 
 fn is_symbol(character: char) -> bool {
@@ -12,7 +16,6 @@ fn is_symbol(character: char) -> bool {
 }
 
 /* isValidPosition and getAdjacentElements based on example from from https://www.geeksforgeeks.org/find-all-adjacent-elements-of-given-element-in-a-2d-array-or-matrix/ */
-
 fn is_valid_position(y: i32, x: i32, y_length: i32, x_length: i32) -> bool {
     y >= 0 && x >= 0 && y < y_length && x < x_length
 }
@@ -44,6 +47,77 @@ fn get_adjacent_elements(characters: &Vec<Vec<char>>, y: i32, x: i32) -> Vec<cha
         adjacent.push(characters[y as usize - 1][x as usize + 1]);
     }
     adjacent
+}
+
+fn is_part_number_digit(input_vec: &Vec<Vec<char>>, y: i32, x: i32) -> bool {
+    let adjacent_elements = get_adjacent_elements(input_vec, y, x);
+
+    for character in adjacent_elements {
+        if is_symbol(character) {
+            return true;
+        }
+    }
+    false
+}
+
+struct Location {
+    start: usize,
+    end: usize,
+}
+
+fn get_number_locations(line: &Vec<char>) -> Vec<Location> {
+    lazy_static! {
+        static ref REGEX: Regex = Regex::new(r"\d+").unwrap();
+    }
+
+    let line_str: String = line.iter().collect();
+    let mut locations: Vec<Location> = Vec::new();
+
+    for field in REGEX.find_iter(&line_str) {
+        locations.push(Location {
+            start: field.start(),
+            end: field.end(),
+        });
+    }
+
+    locations
+}
+
+fn is_valid_part_number(
+    number_location: &Location,
+    line_number: usize,
+    input_vec: &Vec<Vec<char>>,
+) -> bool {
+    for i in number_location.start..number_location.end {
+        if is_part_number_digit(input_vec, line_number as i32, i as i32) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn part_one_sum(input_vec: &Vec<Vec<char>>) -> u32 {
+    let mut sum: u32 = 0;
+    for line_number in 0..input_vec.len() {
+        let number_locations = get_number_locations(&input_vec[line_number]);
+        for location in number_locations {
+            if is_valid_part_number(&location, line_number, &input_vec) {
+                let line_string: String = input_vec[line_number].iter().collect();
+                let number_string: String = line_string
+                    .chars()
+                    .skip(location.start)
+                    .take(location.end - location.start)
+                    .collect();
+
+                sum += number_string
+                    .parse::<u32>()
+                    .expect("Failed to parse number to string");
+            }
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -78,5 +152,45 @@ mod tests {
         assert!(adjacent_elements.contains(&'b'));
         assert!(adjacent_elements.contains(&'g'));
         assert!(adjacent_elements.contains(&'f'));
+    }
+
+    #[test]
+    fn test_is_part_number_digit() {
+        let first_line = vec!['4', '6', '7', '.', '.', '1', '1', '4'];
+        let second_line = vec!['.', '.', '.', '*', '.', '.', '.', '.'];
+        let input_vec = vec![first_line, second_line];
+
+        assert!(is_part_number_digit(&input_vec, 0, 2));
+        assert!(!is_part_number_digit(&input_vec, 0, 5));
+    }
+
+    #[test]
+    fn test_get_number_locations() {
+        let line = vec!['4', '6', '7', '.', '.', '1', '1'];
+        let number_locations = get_number_locations(&line);
+
+        assert_eq!(number_locations.len(), 2);
+        assert_eq!(number_locations[0].start, 0);
+        assert_eq!(number_locations[0].end, 3);
+        assert_eq!(number_locations[1].start, 5);
+        assert_eq!(number_locations[1].end, 7);
+    }
+
+    #[test]
+    fn test_is_valid_part_number() {
+        let first_line = vec!['4', '6', '7', '.', '.', '1', '1', '4'];
+        let second_line = vec!['.', '.', '.', '*', '.', '.', '.', '.'];
+        let input_vec = vec![first_line, second_line];
+
+        assert!(is_valid_part_number(
+            &Location { start: 0, end: 3 },
+            0,
+            &input_vec
+        ));
+        assert!(!is_valid_part_number(
+            &Location { start: 5, end: 8 },
+            0,
+            &input_vec
+        ));
     }
 }
