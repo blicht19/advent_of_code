6695db183a027996906473c466b2c4d777863340
lichtb <baileymlicht@gmail.com>
2024-02-10 16:13:33 -0600

    Day 3 part 2

--- 2023/day_03/src/main.rs
+++ 2023/day_03/src/main.rs
@@ -7,8 +7,10 @@ fn main() {
     let input_vec = get_two_dimensional_vector(file_name.as_str());
 
     let part_one_sum = part_one_sum(&input_vec);
-
     println!("Part 1: {}", part_one_sum);
+
+    let part_two_sum = part_two_sum(&input_vec);
+    println!("Part 2: {}", part_two_sum);
 }
 
 fn is_symbol(character: char) -> bool {
@@ -60,12 +62,13 @@ fn is_part_number_digit(input_vec: &Vec<Vec<char>>, y: i32, x: i32) -> bool {
     false
 }
 
+#[derive(Clone)]
 struct Location {
     start: usize,
     end: usize,
 }
 
-fn get_number_locations(line: &Vec<char>) -> Vec<Location> {
+fn get_number_locations_in_line(line: &Vec<char>) -> Vec<Location> {
     lazy_static! {
         static ref REGEX: Regex = Regex::new(r"\d+").unwrap();
     }
@@ -100,7 +103,7 @@ fn is_valid_part_number(
 fn part_one_sum(input_vec: &Vec<Vec<char>>) -> u32 {
     let mut sum: u32 = 0;
     for line_number in 0..input_vec.len() {
-        let number_locations = get_number_locations(&input_vec[line_number]);
+        let number_locations = get_number_locations_in_line(&input_vec[line_number]);
         for location in number_locations {
             if is_valid_part_number(&location, line_number, &input_vec) {
                 let line_string: String = input_vec[line_number].iter().collect();
@@ -114,6 +117,137 @@ fn part_one_sum(input_vec: &Vec<Vec<char>>) -> u32 {
                     .parse::<u32>()
                     .expect("Failed to parse number to string");
             }
+        }
+    }
+
+    sum
+}
+
+#[derive(Clone)]
+struct TwoDimensionalLocation {
+    row_location: Location,
+    y: usize,
+}
+
+fn get_number_locations(input_vec: &Vec<Vec<char>>) -> Vec<TwoDimensionalLocation> {
+    let mut number_locations: Vec<TwoDimensionalLocation> = Vec::new();
+    for i in 0..input_vec.len() {
+        let locations = get_number_locations_in_line(&input_vec[i]);
+        for location in locations {
+            number_locations.push(TwoDimensionalLocation {
+                y: i,
+                row_location: location,
+            })
+        }
+    }
+
+    number_locations
+}
+
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+fn get_asterisk_coordinates(input_vec: &Vec<Vec<char>>) -> Vec<Coordinate> {
+    let mut locations: Vec<Coordinate> = Vec::new();
+    for y in 0..input_vec.len() {
+        for x in 0..input_vec[y].len() {
+            if input_vec[y][x] == '*' {
+                locations.push(Coordinate { x, y });
+            }
+        }
+    }
+
+    locations
+}
+
+fn get_adjacent_number_locations(
+    coord: Coordinate,
+    number_locations: &Vec<TwoDimensionalLocation>,
+    input_vec: &Vec<Vec<char>>,
+) -> Vec<TwoDimensionalLocation> {
+    let mut adjacent_numbers: Vec<TwoDimensionalLocation> = Vec::new();
+
+    if coord.y >= 1 {
+        let mut above_numbers: Vec<TwoDimensionalLocation> = number_locations
+            .iter()
+            .filter(|location| {
+                location.y == coord.y - 1
+                    && ((location.row_location.start >= coord.x - 1
+                        && location.row_location.start <= coord.x + 1)
+                        || (location.row_location.end > coord.x - 1
+                            && location.row_location.end < coord.x + 2))
+            })
+            .cloned()
+            .collect();
+        adjacent_numbers.append(&mut above_numbers);
+    }
+
+    if coord.x + 1 < input_vec[coord.y].len() {
+        let mut right_numbers: Vec<TwoDimensionalLocation> = number_locations
+            .iter()
+            .filter(|location| location.y == coord.y && location.row_location.start == coord.x + 1)
+            .cloned()
+            .collect();
+        adjacent_numbers.append(&mut right_numbers);
+    }
+
+    if coord.y + 1 < input_vec.len() {
+        let mut below_numbers: Vec<TwoDimensionalLocation> = number_locations
+            .iter()
+            .filter(|location| {
+                location.y == coord.y + 1
+                    && ((location.row_location.start >= coord.x - 1
+                        && location.row_location.start <= coord.x + 1)
+                        || (location.row_location.end > coord.x - 1
+                            && location.row_location.end < coord.x + 2))
+            })
+            .cloned()
+            .collect();
+        adjacent_numbers.append(&mut below_numbers);
+    }
+
+    if coord.x >= 1 {
+        let mut left_numbers: Vec<TwoDimensionalLocation> = number_locations
+            .iter()
+            .filter(|location| location.y == coord.y && location.row_location.end == coord.x)
+            .cloned()
+            .collect();
+        adjacent_numbers.append(&mut left_numbers);
+    }
+
+    adjacent_numbers
+}
+
+fn get_number_from_two_dimensional_location(
+    input_vec: &Vec<Vec<char>>,
+    location: &TwoDimensionalLocation,
+) -> u32 {
+    let line_string: String = input_vec[location.y].iter().collect();
+    let number_string: String = line_string
+        .chars()
+        .skip(location.row_location.start)
+        .take(location.row_location.end - location.row_location.start)
+        .collect();
+
+    number_string
+        .parse::<u32>()
+        .expect("Failed to parse number to string")
+}
+
+fn part_two_sum(input_vec: &Vec<Vec<char>>) -> u32 {
+    let asterisks = get_asterisk_coordinates(&input_vec);
+    let numbers = get_number_locations(&input_vec);
+
+    let mut sum = 0;
+
+    for asterisk in asterisks {
+        let adjacent_numbers = get_adjacent_number_locations(asterisk, &numbers, input_vec);
+
+        if adjacent_numbers.len() == 2 {
+            sum += get_number_from_two_dimensional_location(&input_vec, &adjacent_numbers[0])
+                * get_number_from_two_dimensional_location(&input_vec, &adjacent_numbers[1]);
         }
     }
 
@@ -165,9 +299,9 @@ mod tests {
     }
 
     #[test]
-    fn test_get_number_locations() {
+    fn test_get_number_locations_in_line() {
         let line = vec!['4', '6', '7', '.', '.', '1', '1'];
-        let number_locations = get_number_locations(&line);
+        let number_locations = get_number_locations_in_line(&line);
 
         assert_eq!(number_locations.len(), 2);
         assert_eq!(number_locations[0].start, 0);
@@ -192,5 +326,81 @@ mod tests {
             0,
             &input_vec
         ));
+    }
+
+    #[test]
+    fn test_get_number_locations() {
+        let first_line = vec!['4', '6', '7', '.', '.', '1', '1', '4'];
+        let second_line = vec!['.', '.', '.', '2', '2', '.', '.', '.'];
+        let input_vec = vec![first_line, second_line];
+        let number_locations = get_number_locations(&input_vec);
+
+        assert_eq!(number_locations.len(), 3);
+
+        assert_eq!(number_locations[0].y, 0);
+        assert_eq!(number_locations[0].row_location.start, 0);
+        assert_eq!(number_locations[0].row_location.end, 3);
+
+        assert_eq!(number_locations[1].y, 0);
+        assert_eq!(number_locations[1].row_location.start, 5);
+        assert_eq!(number_locations[1].row_location.end, 8);
+
+        assert_eq!(number_locations[2].y, 1);
+        assert_eq!(number_locations[2].row_location.start, 3);
+        assert_eq!(number_locations[2].row_location.end, 5);
+    }
+
+    #[test]
+    fn test_get_asterisk_coordinates() {
+        let first_line = vec!['4', '6', '7', '.', '*', '1', '1', '4'];
+        let second_line = vec!['.', '.', '.', '*', '.', '.', '.', '.'];
+        let input_vec = vec![first_line, second_line];
+        let coordinates = get_asterisk_coordinates(&input_vec);
+
+        assert_eq!(coordinates.len(), 2);
+
+        assert_eq!(coordinates[0].y, 0);
+        assert_eq!(coordinates[0].x, 4);
+
+        assert_eq!(coordinates[1].y, 1);
+        assert_eq!(coordinates[1].x, 3);
+    }
+
+    #[test]
+    fn test_get_adjacent_number_locations() {
+        let first_line = vec!['4', '6', '7', '.', '*', '1', '1', '4'];
+        let second_line = vec!['.', '.', '.', '*', '.', '.', '.', '.'];
+        let third_line = vec!['.', '.', '3', '5', '.', '.', '.', '.'];
+        let input_vec = vec![first_line, second_line, third_line];
+        let number_locations = get_number_locations(&input_vec);
+
+        let adjacent_numbers =
+            get_adjacent_number_locations(Coordinate { x: 3, y: 1 }, &number_locations, &input_vec);
+
+        assert_eq!(adjacent_numbers.len(), 2);
+
+        assert_eq!(adjacent_numbers[0].y, 0);
+        assert_eq!(adjacent_numbers[0].row_location.start, 0);
+        assert_eq!(adjacent_numbers[0].row_location.end, 3);
+
+        assert_eq!(adjacent_numbers[1].y, 2);
+        assert_eq!(adjacent_numbers[1].row_location.start, 2);
+        assert_eq!(adjacent_numbers[1].row_location.end, 4);
+    }
+
+    #[test]
+    fn test_get_number_from_two_dimensional_location() {
+        let first_line = vec!['4', '6', '7', '.', '*', '1', '1', '4'];
+        let second_line = vec!['.', '.', '.', '*', '.', '.', '.', '.'];
+        let input_vec = vec![first_line, second_line];
+        let location = TwoDimensionalLocation {
+            y: 0,
+            row_location: Location { start: 0, end: 3 },
+        };
+
+        assert_eq!(
+            get_number_from_two_dimensional_location(&input_vec, &location),
+            467
+        );
     }
 }
