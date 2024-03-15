9a0d100202637b5165668a79e34b514609e88f36
lichtb <baileymlicht@gmail.com>
2024-03-14 21:37:13 -0500

    Day 16 clean up part 1 and add part 2

--- 2023/day_16/src/main.rs
+++ 2023/day_16/src/main.rs
@@ -1,28 +1,93 @@
+use core::fmt;
 use std::collections::HashSet;
 
-use json::{object, JsonValue};
+use lazy_static::lazy_static;
 use library::{get_filename_arg, get_two_dimensional_vector};
 use regex::Regex;
 
 fn main() {
     let file_name = get_filename_arg();
-    // let file_name = String::from("resources/test_input.txt");
-    let mut input = get_two_dimensional_vector(&file_name);
+    let input = get_two_dimensional_vector(&file_name);
+
+    println!("Part 1: {}", part_one(&input));
+    println!("Part 2: {}", part_two(&input));
+}
+
+fn part_one(input: &Vec<Vec<char>>) -> usize {
+    let mut input = input.clone();
     let mut beams = HashSet::new();
-
     radiate_right(0, 0, &mut beams, &mut input);
+    get_sum(&input)
+}
 
-    let regex: Regex = Regex::new(r"[#=?>!]").unwrap();
+fn part_two(input: &Vec<Vec<char>>) -> usize {
+    let mut sums = vec![];
+
+    sums.push(get_energized(0, 0, &input, Directions::DOWN));
+    sums.push(get_energized(0, 0, &input, Directions::RIGHT));
+
+    sums.push(get_energized(
+        0,
+        input[0].len() - 1,
+        &input,
+        Directions::DOWN,
+    ));
+    sums.push(get_energized(
+        0,
+        input[0].len() - 1,
+        &input,
+        Directions::LEFT,
+    ));
+
+    sums.push(get_energized(input.len() - 1, 0, &input, Directions::UP));
+    sums.push(get_energized(input.len() - 1, 0, &input, Directions::RIGHT));
+
+    sums.push(get_energized(
+        input.len() - 1,
+        input[0].len() - 1,
+        &input,
+        Directions::UP,
+    ));
+    sums.push(get_energized(
+        input.len() - 1,
+        input[0].len() - 1,
+        &input,
+        Directions::LEFT,
+    ));
+
+    for i in 1..input.len() - 1 {
+        sums.push(get_energized(i, 0, &input, Directions::RIGHT));
+        sums.push(get_energized(
+            i,
+            input[i].len() - 1,
+            &input,
+            Directions::LEFT,
+        ));
+    }
+
+    for i in 1..input[0].len() - 1 {
+        sums.push(get_energized(0, i, &input, Directions::DOWN));
+        sums.push(get_energized(input.len() - 1, i, &input, Directions::UP));
+    }
+
+    sums.into_iter().max().unwrap()
+}
+
+fn get_sum(input: &Vec<Vec<char>>) -> usize {
+    lazy_static! {
+        static ref PATTERN: Regex = Regex::new(r"[#=?>!]").unwrap();
+    }
+
     let mut sum = 0;
     for line in input {
         let row: String = line.iter().collect();
-        let matches: Vec<_> = regex.find_iter(&row).map(|m| m.as_str()).collect();
+        let matches: Vec<_> = PATTERN.find_iter(&row).map(|m| m.as_str()).collect();
         if matches.len() > 0 {
             sum += matches.len();
         }
     }
 
-    println!("{sum}");
+    sum
 }
 
 enum Directions {
@@ -32,15 +97,47 @@ enum Directions {
     RIGHT,
 }
 
-impl From<Directions> for JsonValue {
-    fn from(value: Directions) -> Self {
-        match value {
-            Directions::UP => JsonValue::String(String::from("UP")),
-            Directions::DOWN => JsonValue::String(String::from("DOWN")),
-            Directions::LEFT => JsonValue::String(String::from("LEFT")),
-            Directions::RIGHT => JsonValue::String(String::from("RIGHT")),
-        }
+impl fmt::Display for Directions {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let string_rep = match self {
+            Self::UP => "UP",
+            Self::DOWN => "DOWN",
+            Self::LEFT => "LEFT",
+            Self::RIGHT => "RIGHT",
+        };
+        write!(f, "{string_rep}")
     }
+}
+
+fn beam_string_format(row: usize, column: usize, direction: Directions) -> String {
+    format!("{row},{column},{direction}")
+}
+
+fn beams_contains(
+    row: usize,
+    column: usize,
+    direction: Directions,
+    beams: &mut HashSet<String>,
+) -> bool {
+    let beam = beam_string_format(row, column, direction);
+    if beams.contains(&beam) {
+        true
+    } else {
+        beams.insert(beam);
+        false
+    }
+}
+
+fn can_increment_row(row: usize, input: &Vec<Vec<char>>) -> bool {
+    row < input.len() - 1
+}
+
+fn can_increment_column(row: usize, column: usize, input: &Vec<Vec<char>>) -> bool {
+    column < input[row].len() - 1
+}
+
+fn can_decrement(row_or_column: usize) -> bool {
+    row_or_column > 0
 }
 
 fn radiate_right(
@@ -49,16 +146,8 @@ fn radiate_right(
     beams: &mut HashSet<String>,
     input: &mut Vec<Vec<char>>,
 ) {
-    let beam = object! {
-        row: row,
-        column: column,
-        direction: Directions::RIGHT
-    };
-    let key = json::stringify(beam);
-    if beams.contains(&key) {
+    if beams_contains(row, column, Directions::RIGHT, beams) {
         return;
-    } else {
-        beams.insert(key);
     }
 
     for i in column..input[row].len() {
@@ -67,29 +156,29 @@ fn radiate_right(
             '-' | '=' => input[row][i] = '=',
             '/' | '?' => {
                 input[row][i] = '?';
-                if row > 0 {
+                if can_decrement(row) {
                     radiate_up(row - 1, i, beams, input);
                 }
                 return;
             }
             '\\' | '>' => {
                 input[row][i] = '>';
-                if row < input.len() - 1 {
+                if can_increment_row(row, input) {
                     radiate_down(row + 1, i, beams, input);
                 }
                 return;
             }
             '|' | '!' => {
                 input[row][i] = '!';
-                if row > 0 {
+                if can_decrement(row) {
                     radiate_up(row - 1, i, beams, input);
                 }
-                if row < input.len() - 1 {
+                if can_increment_row(row, input) {
                     radiate_down(row + 1, i, beams, input);
                 }
                 return;
             }
-            _ => panic!("Invalid character"),
+            invalid_char => panic!("Invalid character {invalid_char}"),
         }
     }
 }
@@ -100,16 +189,8 @@ fn radiate_left(
     beams: &mut HashSet<String>,
     input: &mut Vec<Vec<char>>,
 ) {
-    let beam = object! {
-        row: row,
-        column: column,
-        direction: Directions::LEFT
-    };
-    let key = json::stringify(beam);
-    if beams.contains(&key) {
+    if beams_contains(row, column, Directions::LEFT, beams) {
         return;
-    } else {
-        beams.insert(key);
     }
 
     for i in (0..=column).rev() {
@@ -118,29 +199,29 @@ fn radiate_left(
             '-' | '=' => input[row][i] = '=',
             '/' | '?' => {
                 input[row][i] = '?';
-                if row < input.len() - 1 {
+                if can_increment_row(row, input) {
                     radiate_down(row + 1, i, beams, input);
                 }
                 return;
             }
             '\\' | '>' => {
                 input[row][i] = '>';
-                if row > 0 {
+                if can_decrement(row) {
                     radiate_up(row - 1, i, beams, input);
                 }
                 return;
             }
             '|' | '!' => {
                 input[row][i] = '!';
-                if row > 0 {
+                if can_decrement(row) {
                     radiate_up(row - 1, i, beams, input);
                 }
-                if row < input.len() - 1 {
+                if can_increment_row(row, input) {
                     radiate_down(row + 1, i, beams, input);
                 }
                 return;
             }
-            _ => panic!("Invalid character"),
+            invalid_char => panic!("Invalid character {invalid_char}"),
         }
     }
 }
@@ -151,16 +232,8 @@ fn radiate_down(
     beams: &mut HashSet<String>,
     input: &mut Vec<Vec<char>>,
 ) {
-    let beam = object! {
-        row: row,
-        column: column,
-        direction: Directions::DOWN
-    };
-    let key = json::stringify(beam);
-    if beams.contains(&key) {
+    if beams_contains(row, column, Directions::DOWN, beams) {
         return;
-    } else {
-        beams.insert(key);
     }
 
     for i in row..input.len() {
@@ -168,24 +241,24 @@ fn radiate_down(
             '.' | '#' => input[i][column] = '#',
             '-' | '=' => {
                 input[i][column] = '=';
-                if column > 0 {
+                if can_decrement(column) {
                     radiate_left(i, column - 1, beams, input);
                 }
-                if column < input[row].len() - 1 {
+                if can_increment_column(row, column, input) {
                     radiate_right(i, column + 1, beams, input);
                 }
                 return;
             }
             '/' | '?' => {
                 input[i][column] = '?';
-                if column > 0 {
+                if can_decrement(column) {
                     radiate_left(i, column - 1, beams, input);
                 }
                 return;
             }
             '\\' | '>' => {
                 input[i][column] = '>';
-                if column < input[row].len() - 1 {
+                if can_increment_column(row, column, input) {
                     radiate_right(i, column + 1, beams, input);
                 }
                 return;
@@ -193,22 +266,14 @@ fn radiate_down(
             '|' | '!' => {
                 input[i][column] = '!';
             }
-            _ => panic!("Invalid character"),
+            invalid_char => panic!("Invalid character {invalid_char}"),
         }
     }
 }
 
 fn radiate_up(row: usize, column: usize, beams: &mut HashSet<String>, input: &mut Vec<Vec<char>>) {
-    let beam = object! {
-        row: row,
-        column: column,
-        direction: Directions::UP
-    };
-    let key = json::stringify(beam);
-    if beams.contains(&key) {
+    if beams_contains(row, column, Directions::UP, beams) {
         return;
-    } else {
-        beams.insert(key);
     }
 
     for i in (0..=row).rev() {
@@ -216,24 +281,24 @@ fn radiate_up(row: usize, column: usize, beams: &mut HashSet<String>, input: &mu
             '.' | '#' => input[i][column] = '#',
             '-' | '=' => {
                 input[i][column] = '=';
-                if column > 0 {
+                if can_decrement(column) {
                     radiate_left(i, column - 1, beams, input);
                 }
-                if column < input[row].len() - 1 {
+                if can_increment_column(row, column, input) {
                     radiate_right(i, column + 1, beams, input);
                 }
                 return;
             }
             '/' | '?' => {
                 input[i][column] = '?';
-                if column < input[row].len() - 1 {
+                if can_increment_column(row, column, input) {
                     radiate_right(i, column + 1, beams, input);
                 }
                 return;
             }
             '\\' | '>' => {
                 input[i][column] = '>';
-                if column > 0 {
+                if can_decrement(column) {
                     radiate_left(i, column - 1, beams, input);
                 }
                 return;
@@ -241,7 +306,26 @@ fn radiate_up(row: usize, column: usize, beams: &mut HashSet<String>, input: &mu
             '|' | '!' => {
                 input[i][column] = '!';
             }
-            _ => panic!("Invalid character"),
+            invalid_char => panic!("Invalid character {invalid_char}"),
         }
     }
+}
+
+fn get_energized(
+    row: usize,
+    column: usize,
+    input: &Vec<Vec<char>>,
+    direction: Directions,
+) -> usize {
+    let mut input = input.clone();
+    let mut beams = HashSet::new();
+
+    match direction {
+        Directions::UP => radiate_up(row, column, &mut beams, &mut input),
+        Directions::DOWN => radiate_down(row, column, &mut beams, &mut input),
+        Directions::LEFT => radiate_left(row, column, &mut beams, &mut input),
+        Directions::RIGHT => radiate_right(row, column, &mut beams, &mut input),
+    };
+
+    get_sum(&input)
 }
